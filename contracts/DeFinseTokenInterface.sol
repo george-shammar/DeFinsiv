e8165560f7016e684887f5d451b865554e11372c
george-shammar <george.gbenle@outlook.com>
2021-10-15 22:33:51 +0000

    Create burn interface

--- contracts/DeFinseTokenInterface.sol
+++ contracts/DeFinseTokenInterface.sol
@@ -5,5 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 abstract contract DeFinse is ERC20{
+
     function mint(address _user, uint amount) public {}
+
+    function burn(address _user, uint256 _amount) external virtual {}
 }
