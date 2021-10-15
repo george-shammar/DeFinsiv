20d2d67927e067f79d6982a8b35dcb846cf90dea
george-shammar <george.gbenle@outlook.com>
2021-10-15 22:33:13 +0000

    Create mint interface

--- contracts/DeFinseTokenInterface.sol
+++ contracts/DeFinseTokenInterface.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 abstract contract DeFinse is ERC20{
-    
+    function mint(address _user, uint amount) public {}
 }
